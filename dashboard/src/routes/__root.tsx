e80bd896d22b205b31f9f5319351c87493ff20c5
Taylor Kelley <taylorgkelley@outlook.com>
2025-04-03 10:28:23 -0500

    Oops lol

--- dashboard/src/routes/__root.tsx
+++ dashboard/src/routes/__root.tsx
@@ -1,5 +1,5 @@
-import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
-import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
+import { createRootRoute, Link, Outlet } from '@tanstack/react-router';
+import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
 
 export const Route = createRootRoute({
   component: () => (
@@ -7,9 +7,9 @@ export const Route = createRootRoute({
       <div className="flex gap-2 p-2">
         <Link to="/" className="[&.active]:font-bold">
           Home
-        </Link>{" "}
-        <Link to="/about" className="[&.active]:font-bold">
-          About
+        </Link>{' '}
+        <Link to="/forgot-password" className="[&.active]:font-bold">
+          Forgot my password
         </Link>
       </div>
       <hr />
