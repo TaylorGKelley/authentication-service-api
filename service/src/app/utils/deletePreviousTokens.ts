49bc4bbcb20262b9e1379116476972e5f7ea2c17
Taylor Kelley <taylorgkelley@outlook.com>
2025-04-08 13:11:54 -0500

    fix: passing rid instead of refresh token

--- service/src/app/utils/deletePreviousTokens.ts
+++ service/src/app/utils/deletePreviousTokens.ts
@@ -1,16 +1,19 @@
-import { Request } from 'express';
 import extractBearerToken from './extractBearerToken';
 import {
-	deleteAccessTokenEntry,
-	deleteRefreshTokenEntry,
+  deleteAccessTokenEntry,
+  deleteRefreshTokenEntry,
 } from '../useCases/redis/actions';
+import { Request } from 'express';
+import jwt from 'jsonwebtoken';
+import RefreshTokenPayload from '@/domain/types/token/RefreshTokenPayload';
 
 const deletePreviousTokens = async (req: Request) => {
-	const accessToken = extractBearerToken(req);
-	const { refreshToken } = req.cookies;
+  const accessToken = extractBearerToken(req);
+  const { refreshToken } = req.cookies;
+  const { rid } = jwt.decode(refreshToken) as RefreshTokenPayload;
 
-	if (accessToken) await deleteAccessTokenEntry(accessToken);
-	if (refreshToken) await deleteRefreshTokenEntry(refreshToken?.rid);
+  if (accessToken) await deleteAccessTokenEntry(accessToken);
+  if (refreshToken) await deleteRefreshTokenEntry(rid);
 };
 
 export default deletePreviousTokens;
--- service/src/app/utils/hashToken.ts
+++ service/src/app/utils/hashToken.ts
@@ -1,10 +1,11 @@
 import { UUID } from 'crypto';
 import bcrypt from 'bcrypt';
 
-const hashToken = async (token: string | UUID) => {
-  const salt = await bcrypt.genSalt(5);
+const hashToken = async (token: string | UUID): Promise<string> => {
+  const salt = await bcrypt.genSalt(10);
+  const hashedToken = await bcrypt.hash(token?.toString(), salt);
 
-  return await bcrypt.hash(token, salt);
+  return hashedToken;
 };
 
 export default hashToken;
