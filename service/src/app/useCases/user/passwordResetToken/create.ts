bcf283e98fc577bab74e4c36015c3081ee2e4759
Taylor Kelley <taylorgkelley@outlook.com>
2025-04-13 16:13:26 -0500

    refactor: Cleaned up logging and fixed a few bugs

--- service/src/app/useCases/user/passwordResetToken/create.ts
+++ service/src/app/useCases/user/passwordResetToken/create.ts
@@ -1,29 +1,26 @@
 import generateResetToken from '@/app/utils/generateResetToken';
 import { passwordResetTable } from '@/infrastructure/database/schema/passwordReset.schema';
 import { db } from '@/infrastructure/database';
-import findUser from '../findUser';
-import { EmailAddress } from '@/domain/types/mail/Email';
 import { AppError } from '@/domain/entities/AppError';
+import { User } from '@/domain/entities/User';
 
-export const createPasswordResetToken = async (email: EmailAddress) => {
-  const user = await findUser({ email });
+export const createPasswordResetToken = async (user: User) => {
+	if (!user.id) {
+		throw new AppError('A user with that email was not found');
+	} else if (user.googleId || user.githubId) {
+		throw new AppError(
+			'Password reset is not available for Google or GitHub users',
+			400
+		);
+	}
 
-  if (!user.id) {
-    throw new AppError('A user with that email was not found');
-  } else if (user.googleId || user.githubId) {
-    throw new AppError(
-      'Password reset is not available for Google or GitHub users',
-      400
-    );
-  }
+	const token = generateResetToken();
 
-  const token = generateResetToken();
+	await db.insert(passwordResetTable).values({
+		userId: user.id,
+		token,
+		expirationTime: new Date(Date.now() + 15 * 60 * 1000),
+	});
 
-  await db.insert(passwordResetTable).values({
-    userId: user.id,
-    token,
-    expirationTime: new Date(Date.now() + 15 * 60 * 1000),
-  });
-
-  return token;
+	return token;
 };
--- service/src/interfaces/routers/logView.router.ts
+++ service/src/interfaces/routers/logView.router.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express';
 import { getAllLogs } from '../controllers/logView.controller';
+import requireAuth from '../middleware/requireAuth';
+import { role } from '@/domain/static/roleTypes';
+import requireRole from '../middleware/requireRole';
 
 const logViewRouter = Router();
 
-logViewRouter.get('/', getAllLogs);
+logViewRouter.get('/', requireAuth, requireRole(role.reportUser), getAllLogs);
 
 export default logViewRouter;
