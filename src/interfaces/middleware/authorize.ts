6d01da575f8a8f331d6a5631b404ed2feb839c7b
Taylor Kelley <taylorgkelley@outlook.com>
2025-04-25 13:33:08 -0500

    fix: if user doesn't have any permsisions, an error was thrown without optional chaining

--- src/interfaces/middleware/authorize.ts
+++ src/interfaces/middleware/authorize.ts
@@ -7,30 +7,30 @@ import { AppError } from '@/domain/entities/AppError';
 const clientId = process.env.CLIENT_ID! as UUID;
 
 const authorize: (
-	allowedPermissions: string[]
+  allowedPermissions: string[]
 ) => RequestHandler<any, any, any, any> =
-	(allowedPermissions) => async (req, res, next) => {
-		try {
-			if (!req.user) {
-				throw new AppError('Unauthorized', 401);
-			}
+  (allowedPermissions) => async (req, res, next) => {
+    try {
+      if (!req.user) {
+        throw new AppError('Unauthorized', 401);
+      }
 
-			const permissions = await getPermissionsForUserUseCase(
-				(req.user as User).id!
-			);
+      const permissions = await getPermissionsForUserUseCase(
+        (req.user as User).id!
+      );
 
-			const isAllowed = allowedPermissions.some((allowedPermission) =>
-				permissions[clientId].includes(allowedPermission)
-			);
+      const isAllowed = allowedPermissions.some((allowedPermission) =>
+        permissions[clientId]?.includes(allowedPermission)
+      );
 
-			if (!isAllowed && !allowedPermissions.includes('public')) {
-				throw new AppError('Forbidden', 403);
-			}
+      if (!isAllowed && !allowedPermissions.includes('public')) {
+        throw new AppError('Forbidden', 403);
+      }
 
-			next();
-		} catch (error) {
-			next(error);
-		}
-	};
+      next();
+    } catch (error) {
+      next(error);
+    }
+  };
 
 export default authorize;
