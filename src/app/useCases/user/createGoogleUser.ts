af4e801dd2a08e8c31b30a5aced72ba2849f8ccc
Taylor Kelley <taylorgkelley@outlook.com>
2025-04-29 12:45:28 -0500

    feat: adding profile info population on google user creation

--- src/app/useCases/user/createGoogleUser.ts
+++ src/app/useCases/user/createGoogleUser.ts
@@ -1,46 +1,61 @@
 import Role from '@/domain/types/authorization/Role';
-import { UserWithPassword } from '@/domain/entities/User';
+import { UserWithPassword, UserWithProfile } from '@/domain/entities/User';
 import { db } from '@/infrastructure/database';
-import { userTable } from '@/infrastructure/database/schema';
+import { profileInfoTable, userTable } from '@/infrastructure/database/schema';
 
 const createGoogleUser = async (user: {
-	googleId: string;
-	email: string;
-	displayName: string;
-	firstName: string;
-	lastName: string;
+  googleId: string;
+  email: string;
+  firstName: string;
+  lastName: string;
+  photo: string;
 }) => {
-	const newUser = (
-		await db
-			.insert(userTable)
-			.values({
-				...user,
-			})
-			.returning()
-	).at(0) as UserWithPassword;
+  const newUser = (
+    await db
+      .insert(userTable)
+      .values({
+        googleId: user.googleId,
+        email: user.email,
+      })
+      .returning()
+  ).at(0) as UserWithPassword;
 
-	// Insert default role
-	// const defaultRoleId = (
-	//   await db
-	//     .select()
-	//     .from(roleTable)
-	//     .where(eq(roleTable.isDefault, true))
-	//     .limit(1)
-	// ).at(0)?.id;
-	// const userRole = await db
-	//   .insert(userRoleTable)
-	//   .values({ userId: newUser.id, roleId: defaultRoleId || 1 })
-	//   .returning();
-	// const role = await db
-	//   .select()
-	//   .from(roleTable)
-	//   .where(eq(roleTable.id, defaultRoleId || 0));
+  const newProfile = (
+    await db
+      .insert(profileInfoTable)
+      .values({
+        userId: newUser.id,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        profileImage: user.photo,
+      })
+      .returning()
+  ).at(0) as Partial<UserWithProfile>;
 
-	// newUser.roles = [
-	//   { ...userRole.at(0), permissionLevel: role.at(0)?.permissionLevel },
-	// ] as Role[];
+  // Insert default role
+  // const defaultRoleId = (
+  //   await db
+  //     .select()
+  //     .from(roleTable)
+  //     .where(eq(roleTable.isDefault, true))
+  //     .limit(1)
+  // ).at(0)?.id;
+  // const userRole = await db
+  //   .insert(userRoleTable)
+  //   .values({ userId: newUser.id, roleId: defaultRoleId || 1 })
+  //   .returning();
+  // const role = await db
+  //   .select()
+  //   .from(roleTable)
+  //   .where(eq(roleTable.id, defaultRoleId || 0));
 
-	return newUser;
+  // newUser.roles = [
+  //   { ...userRole.at(0), permissionLevel: role.at(0)?.permissionLevel },
+  // ] as Role[];
+
+  const userWithProfile = new UserWithProfile(newUser, newProfile);
+
+  return userWithProfile;
 };
 
 export default createGoogleUser;
--- src/infrastructure/configurations/passport/googleAuthentication.ts
+++ src/infrastructure/configurations/passport/googleAuthentication.ts
@@ -4,31 +4,31 @@ import createGoogleUser from '@/app/useCases/user/createGoogleUser';
 import { AppError } from '@/domain/entities/AppError';
 
 const googleAuthentication: VerifyFunction = async (
-	_accessToken,
-	_refreshToken,
-	profile,
-	done
+  _accessToken,
+  _refreshToken,
+  profile,
+  done
 ) => {
-	try {
-		let user = await findUser({ googleId: profile.id });
+  try {
+    let user = await findUser({ googleId: profile.id });
 
-		if (user?.id === undefined) {
-			user = await createGoogleUser({
-				googleId: profile.id,
-				email: profile.email,
-				displayName: profile.displayName,
-				firstName: profile.name.givenName,
-				lastName: profile.name.familyName,
-			});
-		}
+    if (user?.id === undefined) {
+      user = await createGoogleUser({
+        googleId: profile.id,
+        email: profile.email,
+        firstName: profile.name.givenName,
+        lastName: profile.name.familyName,
+        photo: profile.picture,
+      });
+    }
 
-		if (user?.id === undefined)
-			throw new AppError('Error fetching or creating user', 500);
+    if (user?.id === undefined)
+      throw new AppError('Error fetching or creating user', 500);
 
-		done(null, user);
-	} catch (error) {
-		done(error, null);
-	}
+    done(null, user);
+  } catch (error) {
+    done(error, null);
+  }
 };
 
 export default googleAuthentication;
